e8ff5f4fff0406170055b6c176f6ba28dbf07cef
ImSungHun <ishun0421@daum.net>
2022-09-20 13:37:28 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,9 @@
 def print_hello():
     print("Hello World!!")
-    if __name__ == '__main__':
-        print_hello()
+
+def print_KIT():
+    print("Kumoh National \nInstitute of Technology.")
+
+if __name__ == '__main__':
+    print_KIT()
+    print_hello()
