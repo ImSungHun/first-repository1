23c8dc95ee5e99df95a1f60ca85b479048042574
ImSungHun <ishun0421@daum.net>
2022-09-23 16:09:14 +0900

    four file are commited

---
+++ main.py
@@ -0,0 +1,10 @@
+import max_profit as mp
+import prime_number as pn
+
+if __name__ == '__main__':
+    pn.is_prime_number(1)
+    pn.is_prime_number(13)
+    pn.is_prime_number(24)
+
+    prices = (15, 10, 6, 9, 10, 4)
+    print(mp.maxProfit_bruteforce(prices))
---
+++ max_profit.py
@@ -0,0 +1,11 @@
+def maxProfit_bruteforce (prices):
+   max_price = 0
+
+   for i, price in enumerate(prices):
+       for j in range(i, len(prices)):
+           max_price = max(prices[j] - price, max_price)
+
+   return max_price
+
+prices = (15, 10, 6, 9, 10, 4)
+
